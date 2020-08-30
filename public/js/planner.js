60887e2d28950d6b610f4028bb7a9d06d676eb3e
beknobloch <bknobs4@icloud.com>
2020-08-29 22:40:06 -0400

    More planner

--- public/js/planner.js
+++ public/js/planner.js
@@ -1,22 +1,38 @@
 
-const form = document.getElementById("form-container");
+const destinationContainer = document.getElementById("destination-container")
 const stopNumberElement = document.getElementById("num-of-stops");
 
+let stopTracker = 0;
 let idTracker = 0;
 
 
 function updateForm() {
-    let newRow = document.createElement("div");
-    
-    newRow.classList.add("row");
 
-    newRow.innerHTML = `
-                    <div class="input-field col s12">
-                    <input placeholder="" id="destination-` + toString(idTracker) + `" type="text" class="validate">
-                    <label for="destination-` + toString(idTracker) + `">Destination #` + toString(idTracker + 1) + `</label>
-                    </div>`;
+    if(stopNumberElement.value > stopTracker) {
+        for(i = 0 ; i < stopNumberElement.value - stopTracker ; i++) {
+            let newRow = document.createElement("div");
+            
+            newRow.classList.add("row");
 
-    form.appendChild(newRow);
+            newRow.innerHTML = `
+                            <div class="input-field col s12">
+                            <input placeholder="" id="destination-` + idTracker.toString() + `" type="text" class="validate">
+                            <label for="destination-` + idTracker.toString() + `">Destination #` + (idTracker + 1).toString() + `</label>
+                            </div>`;
+
+            destinationContainer.appendChild(newRow);
+
+            idTracker += 1;
+        }
+    }
+    else {
+        for(i = 0 ; i < stopTracker - stopNumberElement.value ; i ++ ) {
+            destinationContainer.removeChild(destinationContainer.lastChild);
+            idTracker -= 1;
+        }
+    }
+
+    stopTracker = stopNumberElement.value;
 
 }
 
